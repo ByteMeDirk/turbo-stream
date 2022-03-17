8a7dbd65dd7185c8905d05b1d30b88906d61d328
dirksteynberg <dirkscgm@gmail.com>
2022-03-17 11:12:23 +0200

    added file writing utils

--- tests/test_file_handlers.py
+++ tests/test_file_handlers.py
@@ -1,9 +1,10 @@
 """
 Test turbo_stream.utils.file_handlers
 """
+import os
 import unittest
 
-from turbo_stream.utils.file_handlers import load_file
+from turbo_stream.utils.file_handlers import load_file, write_file
 
 
 class TestFileHandlers(unittest.TestCase):
@@ -37,3 +38,35 @@ class TestFileHandlers(unittest.TestCase):
             load_file("tests/assets/config.yaml", "yaml"),
             {"test_config": {"key": "value"}},
         )
+
+    def test_write_file_yml(self):
+        """
+        Test if writer successfully writes to yml file.
+        """
+        write_file(data={"test_config": {"key": "value"}}, file_location="test.yml")
+        self.assertTrue(os.path.isfile("test.yml"))
+        os.remove("test.yml")
+
+    def test_write_file_yaml(self):
+        """
+        Test if writer successfully writes to yaml file.
+        """
+        write_file(data={"test_config": {"key": "value"}}, file_location="test.yaml")
+        self.assertTrue(os.path.isfile("test.yaml"))
+        os.remove("test.yaml")
+
+    def test_write_file_json(self):
+        """
+        Test if writer successfully writes to json file.
+        """
+        write_file(data={"test_config": {"key": "value"}}, file_location="test.json")
+        self.assertTrue(os.path.isfile("test.json"))
+        os.remove("test.json")
+
+    def test_write_file_csv(self):
+        """
+        Test if writer successfully writes to csv file.
+        """
+        write_file(data={"test_config": {"key": "value"}}, file_location="test.csv")
+        self.assertTrue(os.path.isfile("test.csv"))
+        os.remove("test.csv")
--- turbo_stream/utils/file_handlers.py
+++ turbo_stream/utils/file_handlers.py
@@ -4,6 +4,7 @@ File Handler Methods
 import json
 import logging
 
+import pandas as pd
 import yaml
 
 logging.basicConfig(
@@ -15,10 +16,38 @@ def load_file(file_location: str, fmt: str) -> dict:
     """
     Gathers file data from json or yaml.
     """
-    if file_location is not None:
-        if fmt in ["yaml", "yml"]:
-            with open(file_location, "r", encoding="utf-8") as file:
-                return yaml.safe_load(file)
-        elif fmt == "json":
-            with open(file_location, "r", encoding="utf-8") as file:
-                return json.load(file)
+    if fmt in ["yaml", "yml"]:
+        with open(file_location, "r", encoding="utf-8") as file:
+            return yaml.safe_load(file)
+
+    if fmt == "json":
+        with open(file_location, "r", encoding="utf-8") as file:
+            return json.load(file)
+
+    raise ValueError(f"fmt {fmt} is not supported, try yaml, yml or json.")
+
+
+def write_file(data: (dict, list), file_location):
+    """
+    Writes object to json, csv or yaml.
+    """
+    fmt = file_location.split(".")[-1]
+
+    if fmt in ["yaml", "yml"]:
+        with open(file_location, "w", encoding="utf-8") as file:
+            file.write(yaml.dump(data, sort_keys=False))
+
+    elif fmt == "json":
+        with open(file_location, "w", encoding="utf-8") as file:
+            file.write(json.dumps(data))
+
+    elif fmt == "csv":
+        try:
+            df = pd.DataFrame(data)
+            df.to_csv(file_location, header=True, index=True)
+        except ValueError as err:
+            df = pd.DataFrame(list(data))
+            df.to_csv(file_location, header=True, index=True)
+
+    else:
+        raise ValueError(f"fmt {fmt} is not supported, try yaml, yml, csv or json.")
