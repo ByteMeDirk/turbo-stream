df4d14e6d4739ac5e88475a52fe5c5e569077970
dirksteynberg <dirkscgm@gmail.com>
2022-03-17 15:48:08 +0200

    update tests

--- tests/test_google_search_console_reader.py
+++ tests/test_google_search_console_reader.py
@@ -1,11 +1,13 @@
 """
 Test turbo_stream.google_search_console.reader
 """
+import os
 import unittest
 
 import OpenSSL
 import pytest
 from googleapiclient.discovery import build
+from moto import mock_s3
 
 from turbo_stream.google_search_console.reader import GoogleSearchConsoleReader
 
@@ -90,3 +92,36 @@ class TestGoogleSearchConsoleReader(unittest.TestCase):
 
         with pytest.raises(EOFError):
             reader.run_query()
+
+    def test_write_data_to_local(self):
+        """
+        Test if query attempts to run.
+        """
+        reader = GoogleSearchConsoleReader(
+            credentials="tests/assets/mock_gsc_creds.pickle",
+            configuration={"start_date": "today", "end_date": "today"},
+            intro_off=True,
+        )
+        reader._data_set = [{"date": [], "query": []}]
+        reader.write_date_to_local(file_location="tmp.json")
+        self.assertTrue(os.path.isfile("tmp_date.json"))
+        self.assertTrue(os.path.isfile("tmp_query.json"))
+        os.remove("tmp_date.json")
+        os.remove("tmp_query.json")
+
+    @mock_s3
+    def test_write_partition_data_to_s3(self):
+        """
+        Test if query attempts to run.
+        """
+        if not pytest.raises(Exception):
+            reader = GoogleSearchConsoleReader(
+                credentials="tests/assets/mock_gsc_creds.pickle",
+                configuration={"start_date": "today", "end_date": "today"},
+                intro_off=True,
+            )
+            reader._data_set = [{"date": [], "query": []}]
+
+            reader.write_partition_data_to_s3(
+                bucket="my-bucket", path="path", partition="date"
+            )
